540654e4f809a47b329fd2ba72ca9788ef50869d
og! <167083916+ogcreate@users.noreply.github.com>
2025-05-27 19:47:06 +0800

    remove: cleaned uneccessary files & added buttons png

--- ARAMotorhub/src/com/ogcreate/app/controllers/Settings_EditProfile.java
+++ ARAMotorhub/src/com/ogcreate/app/controllers/Settings_EditProfile.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.Node;
--- ARAMotorhub/src/com/ogcreate/app/controllers/Settings_LogOut.java
+++ ARAMotorhub/src/com/ogcreate/app/controllers/Settings_LogOut.java
@@ -9,7 +9,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
